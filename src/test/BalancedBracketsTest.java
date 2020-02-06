a9e2a08e7f32fbf15ee2cf3ec40decf897d8c1fc
Sai <ammu.416@gmail.com>
2020-02-06 11:30:44 -0600

    Completed with 9 test cases

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,19 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
+    BalancedBrackets test_Class;
+
+//    @Before
+//    public void createBracketObject() {
+//        BalancedBrackets test_Class = new BalancedBrackets();
+//    }
 
     //TODO: add tests here
     @Test
@@ -12,5 +21,49 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
+
+    //Testcase3:
+    @Test
+    public void stringWithBalancedBrackets() {
+        assertTrue(test_Class.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    //Testcase4:
+    @Test
+    public void stringWithBalancedBracketInBetween() {
+        assertTrue(test_Class.hasBalancedBrackets("Launch[Code]"));
+    }
+    //Testcase5:
+    @Test
+    public void stringWithBalancedBracketAtStart() {
+        assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    //Testcase6:
+    @Test
+    public void stringWithOnlyOpeningBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("[LaunchCode"));
+    }
+    //TestCase7:
+    @Test
+    public void stringWithOnlyClosingBracket() {
+        assertFalse(test_Class.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    //TestCase8:
+    @Test
+    public void unOrderedBracketsBetweenString() {
+        assertFalse(test_Class.hasBalancedBrackets("Launch]Code["));
+    }
+
+    //TestCase9:
+    @Test
+    public void stringWithBalancedBracketAtEnd() {
+        assertTrue(test_Class.hasBalancedBrackets("LaunchCode[]"));
+    }
 
 }
